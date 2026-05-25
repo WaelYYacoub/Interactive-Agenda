343bade61df498eca2d09d965f384c5301f82f6f
WaelYYacoub <wael.yacoub@gmail.com>
2026-05-25 14:24:21 +0300

    Fix React Router config - remove non-existent Vercel preset

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,6 @@
 import { type Config } from "@react-router/dev/config";
-import { vercelPreset } from "@react-router/vercel";
 
 export default {
-  presets: [vercelPreset()],
   ssr: true,
   appDirectory: "app",
   buildDirectory: "build",
